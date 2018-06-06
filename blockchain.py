598116da1137db12b1e77d60d4f214e555a0b7f5
sayands <sayandsarkar.1997@gmail.com>
2018-06-06 17:14:26 +0300

    Added the mining endpoint

--- blockchain.py
+++ blockchain.py
@@ -117,7 +117,33 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "We will mine a new block"
+    # We run the proof of work algorithm to get the next proof...
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # We must recieve a reqard for finding the proof.
+    # The sender is "0" to signify that this node has mined a new coin
+
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # Forge the new block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': 'New Block Forged',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+
+    return jsonify(response), 200
 
 
 @app.route('/transactions/new', method=['POST'])
@@ -135,7 +161,7 @@ def new_transaction():
 
     response = {'message': f'Transaction will be added to Block {index}'}
 
-    return jsonify(reponse), 201
+    return jsonify(response), 201
 
 
 @app.route('/chain',  method=['GET'])
